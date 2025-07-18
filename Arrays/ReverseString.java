81bde567e786a143e91018e7fddfc8dde8fedf61
prathameshpatil8933 <132357444+prathameshpatil8933@users.noreply.github.com>
2025-07-18 16:00:37 +0530

    Update ReverseString.java

--- Arrays/ReverseString.java
+++ Arrays/ReverseString.java
@@ -1,1 +1,35 @@
+// Problem: https://leetcode.com/problems/reverse-string/description/ <br>
+//Approach:Two Pointers <br>
+//Time: O(n), Space: O(n)
+-------------------------------------------------------------------------------------------
+BRUTE FORCE 
+class Solution {
+    public void reverseString(char[] s) {
+
+    char[] temp = new char[s.length]; 
+    for (int i = 0; i < s.length; i++) {
+        temp[i] = s[s.length - 1 - i]; 
+    }
+    System.arraycopy(temp, 0, s, 0, s.length); // Copy back
+}
+        
+    }
+-------------------------------------------------------------------------------------------
+OPTIMIZED CODE
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
 
